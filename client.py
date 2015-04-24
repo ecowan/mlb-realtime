1e4ebfa503d9db4e3375372b2558bd7df9ae9e47
Ethan <ethan24@gmail.com>
2015-04-23 21:40:58 -0400

    Better logging function

--- client.py
+++ client.py
@@ -6,19 +6,24 @@ from parse import MlbParser
 from time_converter import TimeConverter
 from url_builder import UrlBuilder
 
-logging.basicConfig(level=logging.INFO)
-handler = logging.FileHandler('mlb.log')
-handler.setLevel(logging.INFO)
-logger = logging.getLogger(__name__)
-logger.addHandler(handler)
-logger.info(TimeConverter().get_timestamp() + '\tStart Client')
+
 
 class Client:
 
     def __init__(self, mlb_parser):
         self.mlb_parser = mlb_parser
+        self.logger = None
+
+    def initialize_logger(self):
+        logging.basicConfig(level=logging.INFO)
+        handler = logging.FileHandler('mlb.log')
+        handler.setLevel(logging.INFO)
+        self.logger = logging.getLogger(__name__)
+        self.logger.addHandler(handler)
+        self.logger.info(TimeConverter().get_timestamp() + '\tStart Client')
 
     def update(self):
+        self.initialize_logger()
         threading.Timer(1.25, self.update).start()
         xml = self.mlb_parser.get_xml()
         self.mlb_parser.get_play(xml)
@@ -26,10 +31,10 @@ class Client:
             new_score = self.mlb_parser.get_score(self.mlb_parser.last_play)
             if new_score != self.mlb_parser.score:
                 self.mlb_parser.score = new_score
-                logger.info("%s\t%s", self.mlb_parser.last_play.findall('string')[-1].text, self.mlb_parser.score['home'])
+                self.logger.info("%s\t%s", self.mlb_parser.last_play.findall('string')[-1].text, self.mlb_parser.score['home'])
         else:
             self.mlb_parser.score = self.mlb_parser.get_score(self.mlb_parser.last_play)
-            logger.info("%s\thome:\t%s\taway:\t%s", self.mlb_parser.last_play.findall('string')[-1].text, self.mlb_parser.score['home'], self.mlb_parser.score['away'])
+            self.logger.info("%s\thome:\t%s\taway:\t%s", self.mlb_parser.last_play.findall('string')[-1].text, self.mlb_parser.score['home'], self.mlb_parser.score['away'])
 
 
 def main():
@@ -39,8 +44,8 @@ def main():
     c = Client(mlb)
     c.update()
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
 
 
 # mlb = MlbParser("http://gd2.mlb.com/components/game/mlb/year_2015/month_04/day_21/gid_2015_04_21_slnmlb_wasmlb_1/runScoringPlays.plist")
