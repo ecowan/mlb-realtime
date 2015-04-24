4be8c27484ef67276b52fb75007377ab02d11c85
Ethan <ethan24@gmail.com>
2015-04-23 21:21:41 -0400

    command line function

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.0',
+    version='0.0.3',
 
     description='Real time baseball updates',
 
@@ -94,7 +94,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'mlb_realtime=mlb-realtime.client:main',
+            'mlb_realtime=command_line.client:main',
         ],
     },
 )
